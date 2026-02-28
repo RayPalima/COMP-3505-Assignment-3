0b034f28075ff1dee593ae2a4393e89b689d197d
Program606 <pmanz282@mtroyal.ca>
2026-02-28 15:18:13 -0700

    Improved Test Cases, Added Test Annotation to missing

--- JFreeChart_A3/test/org/jfree/data/StringTest.java
+++ JFreeChart_A3/test/org/jfree/data/StringTest.java
@@ -1,46 +1,36 @@
 package org.jfree.data;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class StringTest {
 
 	@ParameterizedTest
 	@CsvSource({
 	  "5.0, 10.0",
-	})
-	void toString_Format_Pos_Pos(double lower, double upper) {
-	  Range exampleRange = new Range(lower, upper);
-	  assertEquals("Range[" + lower + "," + upper + "]", exampleRange.toString());
-	}
-	@ParameterizedTest
-	@CsvSource({
 	  "-3.0, 10.0",
+	  "-10.0, -8.0",
+	  "0.0, 0.0",
 	})
-	void toString_Format_Pos_Neg(double lower, double upper) {
+	void toString_Format_ValidRanges_Test(double lower, double upper) {
 	  Range exampleRange = new Range(lower, upper);
 	  assertEquals("Range[" + lower + "," + upper + "]", exampleRange.toString());
 	}
+	
 	@ParameterizedTest
 	@CsvSource({
 	  "5.0, -10.0",
+	  "-8.0, -10.0"
 	})
-	void toString_Format_Neg_Pos(double lower, double upper) {
-	  Range exampleRange = new Range(lower, upper);
-	  assertEquals("Range[" + lower + "," + upper + "]", exampleRange.toString());
+	void toString_Format_LowerGreaterThanUpper_Test(double lower, double upper) {
+		  assertThrows(IllegalArgumentException.class, () -> new Range(lower, upper));
 	}
-	@ParameterizedTest
-	@CsvSource({
-	  "-8.0, -10.0",
-	})
-	void toString_Format_(double lower, double upper) {
-	  Range exampleRange = new Range(lower, upper);
-	  assertEquals("Range[" + lower + "," + upper + "]", exampleRange.toString());
+	@Test
+	void toString_NegativeZero() {
+	  Range r = new Range(-0.0, 0.0);
+	  assertEquals("Range[-0.0,0.0]", r.toString());
 	}
-	
-	
-
 
 }
